27cbc8e6ba0d84b441cdc67c6afd9379d791ebfe
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2019-06-13 16:17:26 -0300

    Creates tests to lap constructor entity

---
+++ src/domain/lap.py
@@ -0,0 +1,6 @@
+class Lap:
+    def __init__(self, hour_of_lap, laptime, number, average_speed):
+        self.hour_of_lap = hour_of_lap
+        self.laptime = laptime
+        self.number = number
+        self.average_speed = average_speed
---
+++ tests/domain/test_lap.py
@@ -0,0 +1,28 @@
+from datetime import time
+
+from assertpy import assert_that, fail
+import pytest
+
+from src.domain.lap import Lap
+
+class TestLap:
+    @pytest.fixture
+    def lap(self):
+        return Lap(**{
+            'hour_of_lap': '23:49:08.277',
+            'number': '1',
+            'laptime': '1:02.852',
+            'average_speed': '44,275'
+        })
+
+    def test__init__given_string_hour__it_should_be_of_type_time(self, lap):
+        assert_that(lap.hour_of_lap).is_type_of(time)
+
+    def test__init__given_string_number__it_should_be_of_type_integer(self, lap):
+        assert_that(lap.number).is_type_of(int)
+
+    def test__init__given_laptime_string__it_should_be_of_type_time(self, lap):
+        assert_that(lap.laptime).is_type_of(time)
+
+    def test__init__given_average_speed_string__it_should_be_of_type_float(self, lap):
+        assert_that(lap.average_speed).is_type_of(float)
