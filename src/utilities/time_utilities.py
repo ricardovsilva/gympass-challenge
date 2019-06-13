e645a79d755e2726bdd688435ca9900cf0c7edf7
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2019-06-13 18:41:49 -0300

    Implements time_utilities

--- src/utilities/time_utilities.py
+++ src/utilities/time_utilities.py
@@ -1,2 +1,16 @@
 def timestr_to_integer(time):
-    pass
+    time_parts = time.split(':')
+    minutes = int(time_parts[0])
+    remaining = time_parts[-1].split('.')
+    seconds = int(remaining[0])
+    milliseconds = int(remaining[-1])
+
+    return (((minutes * 60) + seconds) * 1000) + milliseconds
+
+def integer_to_timestr(integer_time):
+    milliseconds = integer_time % 1000
+    remaining = (integer_time - milliseconds)/1000
+    seconds = remaining % 60
+    minutes = (remaining - seconds)/60
+
+    return f"{int(minutes)}:{'%02d' % seconds}.{'%03d' % milliseconds}"
