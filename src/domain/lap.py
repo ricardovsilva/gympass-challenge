a9064dd6061879b034cdf9caacd833a7ee15a146
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2019-06-13 18:44:15 -0300

    Implements entity lap.py

--- src/domain/lap.py
+++ src/domain/lap.py
@@ -1,6 +1,8 @@
+from src.utilities.time_utilities import timestr_to_integer
+
 class Lap:
     def __init__(self, hour_of_lap, laptime, number, average_speed):
-        self.hour_of_lap = hour_of_lap
-        self.laptime = laptime
-        self.number = number
-        self.average_speed = average_speed
+        self.hour_of_lap = timestr_to_integer(hour_of_lap)
+        self.laptime = timestr_to_integer(laptime)
+        self.number = int(number)
+        self.average_speed = float(average_speed.replace(',', '.'))
